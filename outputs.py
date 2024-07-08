da8e3965516a5fb5230e5455d079e9f3a29a7a6d
Yasho-Bapat <f20210006@goa.bits-pilani.ac.in>
2024-07-08 23:44:13 +0530

    commit

--- outputs.py
+++ outputs.py
@@ -1,5 +1,4 @@
 from pydantic.v1 import BaseModel, Field
-import datetime
 from typing import List
 
 
@@ -19,63 +18,96 @@ class Identification(BaseModel):
     revision_date: str = Field(description="Revision Date")
 
 
-# COMPOSITION INFORMATION
-class ChemicalInformation(BaseModel):
-    name: str = Field(description="Chemical Name")
-    cas_number: str = Field(description="CAS Number")
-    concentration: str = Field(description="Concentration if given in percentage")
-    other_information: str = Field(description="Other Information regarding chemical only from document provided")
-
-
-class MaterialComposition(BaseModel):
-    composition: List[ChemicalInformation] = Field(description="Composition of the material as a list of ChemicalInformations")
-
-
-# STABILITY AND REACTIVITY
-class StabilityReactivity(BaseModel):
-    reactivity: str = Field(description="Reactivity of the material as given in document")
-    stability: str = Field(description="Stability of the material as given in document")
-    avoid: List[str] = Field(description="List of conditions to avoid as given in document")
-    decomp_products: List[str] = Field(description="List of hazardous decomposition products as given in document")
-    hazardous_polymerization: bool = Field(description="Hazardous polymerization as given in document")
-
-
-# REGULATORY INFORMATION
-class Regulatory(BaseModel):
-    countries: List[str] = Field(description="List of countries the material is regulated in according to the document")
-    agency: List[str] = Field(description="List of organizations the material is in accordance with")
-
-
 # TOXICOLOGICAL INFORMATION
+class ToxicologicalEffect(BaseModel):
+    status: bool = Field(description="Toxic Status")
+    details: str = Field(description="Details or Reason for status (ex - under which regulation or agency a material has a toxic status for an element). Try to provide a valid reason for the status above.")
+    source: str = Field(description="Whether the source was the MSDS information or openai options - (MSDS, OPENAI)")
+
+
+# class SVHC(BaseModel):
+#     status: bool = Field(description="SVHC Status (true or false)")
+#     reason_of_inclusion: List[str] = Field(description="If SVHC status is true, give reason(s) for inclusion. The "
+#                                                        "reasons can only be among - Toxic for reproduction (Article 57c),"
+#                                                        "vPvB (Article 57e), PBT (Article 57d), Equivalent level of "
+#                                                        "concern having probable serious effects to human health "
+#                                                        "(Article 57(f) - human health), Equivalent level of concern "
+#                                                        "having probable serious effects to human health (Article 57(f) - environment, Endocrine disrupting properties (Article 57(f) - human health), Endocrine disrupting properties (Article 57(f) - environment), Respiratory sensitising properties (Article 57(f) - human health), Respiratory sensitising properties (Article 57(f) - environment, Specific target organ toxicity after repeated exposure (Article 57(f) - human health), Mutagenic (Article 57b).")
+#     source: str = Field(description="Whether the source was the MSDS information or openai options - (MSDS, OPENAI)")
+
+
+class Toxicity(BaseModel):
+    action: str = Field(description="Route of how the chemical affects humans or animal; eg: inhalation, touch, etc.")
+    affected: str = Field(description="Affected entity (ex - Human, Rat, Rabbit, etc")
+    signs_and_symptoms: List[str] = Field(description="Sign and symptoms which one observes.")
+    chemical_carcinogenic: ToxicologicalEffect = Field(description="Carcinogenic status of the chemical")
+    chemical_mutagenicity: ToxicologicalEffect = Field(description="Mutagenic status of the chemical")
+    chemical_reproductive_toxicity: ToxicologicalEffect = Field(description="Reproductive Toxicity status of the chemical")
+    chemical_teratogenicity: ToxicologicalEffect = Field(description="Teratogenicity status of the chemical")
+    other_info: List[str] = Field(description="Any other information about the chemical's toxicity which does not "
+                                              "conform to action and signs & symptoms")
+
+
 class ChemicalToxicity(BaseModel):
     name: str = Field(description="name of chemical or component")
-    toxicity: List[str] = Field(description="list of ways or standards a component can be toxic, ex - LD 50 (ORAL-RAT)")
+    toxicity: Toxicity = Field(description="list of ways or standards a component can be toxic, ex - LD 50 (ORAL-RAT)")
+    impacts_on_humans: List[str] = Field(description="list of ways or standards a component can impact humans")
 
 
 class ToxicologicalInfo(BaseModel):
-    toxicity: List[ChemicalToxicity] = Field(description="list of ways component can be toxic")
-    other: List[str] = Field(description="List of additional points relevant to toxicity")
+    material_carcinogenic: ToxicologicalEffect = Field(description="Carcinogenic status of the material")
+    material_mutagenicity: ToxicologicalEffect = Field(description="Mutagenic status of the material")
+    material_reproductive_toxicity: ToxicologicalEffect = Field(description="Reproductive Toxicity status of the material")
+    material_teratogenicity: ToxicologicalEffect = Field(description="Teratogenicity status of the material")
+    chemical_level_toxicity: List[ChemicalToxicity] = Field(description="list of ways component can be toxic")
+    # svhc: str = Field(description="Whether the component is a Substance of Very High Concern. Make the Decision either based on the context or external knowledge sources like ECHA SVHC list.")
+    additional_information: List[str] = Field(description="List of additional points relevant to toxicity")
+
+#
+# # ECO-TOXICITY
+# class EcoToxicity(BaseModel):
+#     name: str = Field(description="name of chemical or component")
+#     toxicity: List[str] = Field(description="list of ways or standards a component can be toxic, ex - LD 50 (ORAL-RAT)")
+#
+#
+# class EcoToxicologicalInfo(BaseModel):
+#     toxicity: List[ChemicalToxicity] = Field(description="list of ways component can be toxic")
+#     other: List[str] = Field(description="List of additional points relevant to ecological toxicity")
 
 
-# ECO-TOXICITY
-class EcoToxicity(BaseModel):
-    name: str = Field(description="name of chemical or component")
-    toxicity: List[str] = Field(description="list of ways or standards a component can be toxic, ex - LD 50 (ORAL-RAT)")
-
-
-class EcoToxicologicalInfo(BaseModel):
-    toxicity: List[ChemicalToxicity] = Field(description="list of ways component can be toxic")
-    other: List[str] = Field(description="List of additional points relevant to ecological toxicity")
-
-
-# DECISIONS
-class Decisions(BaseModel):
-    pfas: bool = Field(description="whether the material is PFAS compliant or not")
-    carcinogenic: bool = Field(description="whether the material is carcinogenic or not")
-    svhc: bool = Field(description="whether the material is SVHC compliant or not")
-
-
-# ALL INFO
-class DocumentInformation(BaseModel):
-    identification: Identification = Field(description="information for identifying the material")
-    material_composition: MaterialComposition = Field(description="information about composition of material")
+# # COMPOSITION INFORMATION
+# class ChemicalInformation(BaseModel):
+#     name: str = Field(description="Chemical Name")
+#     cas_number: str = Field(description="CAS Number")
+#     concentration: str = Field(description="Concentration if given in percentage")
+#     other_information: str = Field(description="Other Information regarding chemical only from document provided")
+#
+#
+# class MaterialComposition(BaseModel):
+#     composition: List[ChemicalInformation] = Field(description="Composition of the material as a list of ChemicalInformations")
+#
+#
+# # STABILITY AND REACTIVITY
+# class StabilityReactivity(BaseModel):
+#     reactivity: str = Field(description="Reactivity of the material as given in document")
+#     stability: str = Field(description="Stability of the material as given in document")
+#     avoid: List[str] = Field(description="List of conditions to avoid as given in document")
+#     decomp_products: List[str] = Field(description="List of hazardous decomposition products as given in document")
+#     hazardous_polymerization: bool = Field(description="Hazardous polymerization as given in document")
+#
+#
+# # REGULATORY INFORMATION
+# class Regulatory(BaseModel):
+#     countries: List[str] = Field(description="List of countries the material is regulated in according to the document")
+#     agency: List[str] = Field(description="List of organizations the material is in accordance with")
+# # DECISIONS
+# class Decisions(BaseModel):
+#     pfas: bool = Field(description="whether the material is PFAS compliant or not")
+#     carcinogenic: bool = Field(description="whether the material is carcinogenic or not")
+#     svhc: bool = Field(description="whether the material is SVHC compliant or not")
+#
+#
+# # ALL INFO
+# class DocumentInformation(BaseModel):
+#     identification: Identification = Field(description="information for identifying the material")
+#     material_composition: MaterialComposition = Field(description="information about composition of material")
--- retrieve.py
+++ retrieve.py
@@ -3,12 +3,14 @@ from langchain_openai.embeddings import AzureOpenAIEmbeddings
 from langchain_openai import AzureChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers.openai_functions import JsonOutputFunctionsParser
-import json, time, os
+import json, time, os, dotenv
 
 from langchain_community.callbacks import get_openai_callback
 
 from langchain_core.utils.function_calling import convert_to_openai_function
 from outputs import MaterialComposition, DocumentInformation, Identification
+
+dotenv.load_dotenv()
 
 ENDPOINT = os.getenv("ENDPOINT")
 OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION")
@@ -16,13 +18,12 @@ AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
 embedding_function = AzureOpenAIEmbeddings(deployment="langchain-splitting-test1", api_key=AZURE_OPENAI_API_KEY, azure_endpoint=ENDPOINT)
 llm = AzureChatOpenAI(deployment_name="langchain-askvai-test-4o", api_key=AZURE_OPENAI_API_KEY, azure_endpoint=ENDPOINT, api_version=OPENAI_API_VERSION)
 
-
 db = Chroma(persist_directory="./chroma_db", collection_name="semantic", embedding_function=embedding_function)
 
-
-material_function = [convert_to_openai_function(MaterialComposition)]
+# material_function = [convert_to_openai_function(MaterialComposition), convert_to_openai_function(Identification)]
 id_function = [convert_to_openai_function(Identification)]
-materialmodel = llm.bind(functions=material_function, function_call={"name": "MaterialComposition"})
+material_function = [convert_to_openai_function(DocumentInformation)]
+materialmodel = llm.bind_functions(functions=material_function)
 idmodel = llm.bind(functions=id_function, function_call={"name": "Identification"})
 
 # Placeholder
@@ -32,7 +33,7 @@ the user. Return output as a JSON object.
 """
 prompt = ChatPromptTemplate.from_messages([
     ("system", template),
-    ("human", "context: {docs}, query: {query}" )
+    ("human", "context: {docs}, query: {query}")
 ])
 
 parser = JsonOutputFunctionsParser()
@@ -40,7 +41,7 @@ parser = JsonOutputFunctionsParser()
 material_chain = prompt | materialmodel | parser
 id_chain = prompt | idmodel | parser
 
-material_query = "Return material composition in document as list of chemicals and identification-related information from the document, including last date of revision"
+material_query = "Return material composition and identification information in the document"
 docs = db.similarity_search(material_query)
 docus = [doc.page_content for doc in docs]
 # print(docus)
@@ -54,19 +55,19 @@ with get_openai_callback() as cb:
     costs.append(cb.total_cost)
     tokens.append(cb.total_tokens)
 
-id_query = "Return section on material identification"
-docs = db.similarity_search(id_query)
-documents = [doc.page_content for doc in docs]
-print("retrieved stuff for id query")
-with get_openai_callback() as cb:
-    res["identification"] = id_chain.invoke({"docs": documents, "query": material_query})
-    costs.append(cb.total_cost)
-    tokens.append(cb.total_tokens)
+# id_query = "Return section on material identification"
+# docs = db.similarity_search(id_query)
+# documents = [doc.page_content for doc in docs]
+# print("retrieved stuff for id query")
+# with get_openai_callback() as cb:
+#     res["identification"] = id_chain.invoke({"docs": documents, "query": material_query})
+#     costs.append(cb.total_cost)
+#     tokens.append(cb.total_tokens)
 
 res["total_cost"] = sum(costs)
 res["total_tokens"] = sum(tokens)
 
-with open("result.json", "w") as f:
+with open("yashoresult.json", "w") as f:
     json.dump(res, f, indent=4)
 
 
