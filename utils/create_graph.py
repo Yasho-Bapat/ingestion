9f53f0a539d130c3cbfba30d336b18292d78f9a4
Yasho-Bapat <f20210006@goa.bits-pilani.ac.in>
2024-07-16 10:53:21 +0530

    graph testing

--- utils/create_graph.py
+++ utils/create_graph.py
@@ -1,4 +1,4 @@
-import os, dotenv
+import os, dotenv, time
 from langchain_community.graphs import Neo4jGraph
 from langchain_openai import AzureChatOpenAI
 from langchain_experimental.graph_transformers import LLMGraphTransformer
@@ -10,11 +10,15 @@ class LangGraphProc:
         os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY")
         self.graph = Neo4jGraph()
         self.llm = AzureChatOpenAI(temperature=0, deployment_name=os.getenv("LLM_DEPLOYMENT"))
-        self.llm_transformer = LLMGraphTransformer(llm=self.llm)
+        self.llm_transformer = LLMGraphTransformer(llm=self.llm,
+                                                   allowed_nodes=["Chemical", "Carcinogen info", "Manufacturer Name"],
+                                                   allowed_relationships=["CONTAINS_CHEMICAL", "IS_CARCINOGEN", "MANUFACTURED_BY"])
 
     def create_graph_documents(self, doc):
         print("create graph called")
+        rn = time.perf_counter()
         graph_documents = self.llm_transformer.convert_to_graph_documents(doc)
+        print(f"Graph creation took {time.perf_counter() - rn} seconds")
         print(f"Nodes:{graph_documents[0].nodes}")
         print(f"Relationships:{graph_documents[0].relationships}")
         self.graph.add_graph_documents(graph_documents)
