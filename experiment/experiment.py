6c779bd8e249887561b204f23993caf39a98d06e
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2024-07-17 15:00:05 +0530

    Graph testing (#1)
    * graph testing
    
    * clean up
    
    ---------
    
    Co-authored-by: Yasho-Bapat <f20210006@goa.bits-pilani.ac.in>

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 from time import time
 
 from main import DocumentProcessor
@@ -7,12 +7,19 @@ from utils.store_results import store_results
 
 if __name__ == "__main__":
     directory_path = "test_docs"
-    processor = DocumentProcessor(documents_directory=directory_path, persist_directory="experiment_chroma_db", log_file="logs/ingestion_service_experiment.log", chunking_method="semantic", store_to_neo4j=False)
+    processor = DocumentProcessor(documents_directory=directory_path, persist_directory="experiment_chroma_db", log_file="logs/ingestion_service_experiment.log", chunking_method="semantic")
     files = os.listdir(directory_path)
     files = [f for f in files if os.path.isfile(os.path.join(directory_path, f))]
     timestamp = str(int(time()))
-    for file in files:
-        print(f"Processing {file}...")
-        processor.logger.info(f"Submitting {file} for processing...")
-        results = processor.run(file)
-        print(store_results(filename=f"json_dump/result_{timestamp}.json", results=results, logger=processor.logger))
+    index = random.randint(0, len(files)-1)
+    # for file in files:
+    #     print(f"Processing {file}...")
+    #     processor.logger.info(f"Submitting {file} for processing...")
+    #     results = processor.run(file)
+    #     print(store_results(filename=f"json_dump/result_{timestamp}.json", results=results, logger=processor.logger))
+
+    file = files[index]
+    print(f"Processing {file}...")
+    processor.logger.info(f"Submitting {file} for processing...")
+    results = processor.run(file)
+    print(store_results(filename=f"json_dump/result_{timestamp}.json", results=results, logger=processor.logger))
--- utils/create_graph.py
+++ utils/create_graph.py
@@ -1,20 +1,31 @@
-import os, dotenv
+import os, dotenv, time
 from langchain_community.graphs import Neo4jGraph
 from langchain_openai import AzureChatOpenAI
 from langchain_experimental.graph_transformers import LLMGraphTransformer
 
+from constants import Constants
+
 dotenv.load_dotenv()
+
 
 class LangGraphProc:
     def __init__(self):
         os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY")
         self.graph = Neo4jGraph()
-        self.llm = AzureChatOpenAI(temperature=0, deployment_name=os.getenv("LLM_DEPLOYMENT"))
-        self.llm_transformer = LLMGraphTransformer(llm=self.llm)
+        self.llm = AzureChatOpenAI(temperature=0, deployment_name=Constants.llm_deployment, azure_endpoint=Constants.endpoint)
+        self.llm_transformer = LLMGraphTransformer(
+            llm=self.llm,
+            allowed_nodes=["Chemical", "Manufacturer Name"],
+            allowed_relationships=["CONTAINS_CHEMICAL", "MANUFACTURED_BY"],
+            node_properties=True,
+            strict_mode=False
+        )
 
     def create_graph_documents(self, doc):
         print("create graph called")
+        rn = time.perf_counter()
         graph_documents = self.llm_transformer.convert_to_graph_documents(doc)
+        print(f"Graph creation took {time.perf_counter() - rn} seconds")
         print(f"Nodes:{graph_documents[0].nodes}")
         print(f"Relationships:{graph_documents[0].relationships}")
         self.graph.add_graph_documents(graph_documents)
